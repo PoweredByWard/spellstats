ebb77ac02451bf3a32d0ec2c3b5c06074e761845
PoweredByWard <Ward@users.noreply.github.com>
2020-12-25 15:17:42 +0100

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,22 @@
 import React from "module";
 import "./App.css";
 
-function Footer(prefabs){
-    var today = new Date();
-    return(
-        <div className="footer">
-            <p>{`© ${today.getFullYear()} SpellStats`}</p>
-        </div>
-    )
+function Footer(prefabs) {
+  var today = new Date();
+  return (
+    <div className="footer">
+      <ul className="footer-container">
+        <li><a
+          href="https://twitter.com/messages/compose?recipient_id=1341518237230641159"
+          class="twitter-dm-button"
+          data-screen-name="@SpellStats"
+        >
+          Contact us
+        </a></li>
+      </ul>
+      <p>{`© ${today.getFullYear()} SpellStats. All Rights Reserved`}</p>
+    </div>
+  );
 }
 
 export default Footer;
