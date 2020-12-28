9ee8ba2bc94d6076426403a06883ba736eba0e04
PoweredByWard <Ward@users.noreply.github.com>
2020-12-28 15:27:18 +0100

    homepage update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,30 @@
 import React from "module";
-import "./App.css";
+import "./Footer.css";
 
 function Footer(prefabs) {
   var today = new Date();
   return (
     <div className="footer">
-      <ul className="footer-container">
-        <li><a
-          href="https://twitter.com/messages/compose?recipient_id=1341518237230641159"
-          class="twitter-dm-button"
-          data-screen-name="@SpellStats"
-        >
-          Contact us
-        </a></li>
-      </ul>
-      <p>{`© ${today.getFullYear()} SpellStats. All Rights Reserved`}</p>
+      <div className="footer-container">
+        <div className="footer-top">
+          <span>{`© ${today.getFullYear()} SpellStats`}</span>
+          <div className="footer-top-right">
+            <a
+              href="https://twitter.com/messages/compose?recipient_id=1341518237230641159"
+              class="footer-item"
+              data-screen-name="@SpellStats"
+            >Support</a>
+            <a
+              href="https://twitter.com/messages/compose?recipient_id=1341518237230641159"
+              class="footer-item"
+              data-screen-name="@SpellStats"
+            >Jobs</a>
+          </div>
+        </div>
+        <p className="footer-description">
+        SpellStats isn't endorsed by Proletariat and doesn't reflect the views or opinions of Proletariat or anyone officially involved in producing or managing Proletariat properties. Proletariat, and all associated properties are trademarks or registered trademarks of Proletariat, Inc.
+        </p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Classes from "./components/Classes";
 import comingSoon from "./components/ComingSoon";
+import home from "./components/Home";
 import Overview from "./components/Overview";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -11,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-      <Route path="/" exact component={comingSoon}/>
+      <Route path="/" exact component={home}/>
       <Route path="/classes" component={Classes}/>
       <Route path="/overview" component={Overview}/>
       </Switch>
