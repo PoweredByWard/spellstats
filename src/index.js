39f22d95bc522834717d6236882d9cf0ec2bf55d
PoweredByWard <Ward@users.noreply.github.com>
2020-12-28 15:30:23 +0100

    homepage update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Classes from "./components/Classes";
-import comingSoon from "./components/ComingSoon";
-import home from "./components/Home";
-import Overview from "./components/Overview";
+//import Classes from "./components/Classes";
+//import comingSoon from "./components/ComingSoon";
+import Home from "./components/Home";
+//import Overview from "./components/Overview";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -12,9 +12,11 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-      <Route path="/" exact component={home}/>
-      <Route path="/classes" component={Classes}/>
-      <Route path="/overview" component={Overview}/>
+        <Route path="/" exact component={Home} />
+        {
+          //<Route path="/classes" component={Classes}/>
+          //<Route path="/overview" component={Overview}/>
+        }
       </Switch>
     </Router>
   </React.StrictMode>,
